7787d1f1352f40b33baca5f50d97c661ecbb6278
robinx51 <kabachek.nikita@mail.ru>
2025-04-14 14:36:34 +0300

    Initial commit

---
+++ src/main/java/ru/journal/ExchangeRateJournalApplication.java
@@ -0,0 +1,13 @@
+package ru.journal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchangeRateJournalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExchangeRateJournalApplication.class, args);
+	}
+
+}
